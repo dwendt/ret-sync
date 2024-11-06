1d4bcfecc1386d6093484d77afb48b02e2bd33ee
Christophe Alladoum <christophe.alladoum@elastic.co>
2024-11-05 16:43:41 -0800

    qt6 compat

--- ext_bn/retsync/__init__.py
+++ ext_bn/retsync/__init__.py
@@ -27,7 +27,8 @@ SOFTWARE.
 from collections import namedtuple
 
 import binaryninjaui
-if 'qt_major_version' in binaryninjaui.__dict__ and binaryninjaui.qt_major_version == 6:
+
+if "qt_major_version" in binaryninjaui.__dict__ and binaryninjaui.qt_major_version == 6:
     from PySide6.QtCore import Qt
     from PySide6.QtGui import QKeySequence
 else:
@@ -41,25 +42,43 @@ from .retsync.rsconfig import rs_log
 
 
 def add_commands(plugin):
-    DbgAction = namedtuple('DbgAction', 'name, key_seq, handler')
+    DbgAction = namedtuple("DbgAction", "name, key_seq, handler")
     plugin_actions = (
-        DbgAction("SyncEnable", QKeySequence(Qt.ALT + Qt.Key_S), UIAction(plugin.cmd_sync)),
-        DbgAction("SyncDisable", QKeySequence(Qt.ALT + Qt.SHIFT + Qt.Key_S), UIAction(plugin.cmd_syncoff)),
-        DbgAction("SyncGo", QKeySequence(Qt.ALT + Qt.Key_F5), UIAction(plugin.cmd_go)),
+        DbgAction(
+            "SyncEnable", QKeySequence(Qt.ALT | Qt.Key_S), UIAction(plugin.cmd_sync)
+        ),
+        DbgAction(
+            "SyncDisable",
+            QKeySequence(Qt.ALT | Qt.SHIFT | Qt.Key_S),
+            UIAction(plugin.cmd_syncoff),
+        ),
+        DbgAction("SyncGo", QKeySequence(Qt.ALT | Qt.Key_F5), UIAction(plugin.cmd_go)),
         DbgAction("SyncStepOver", QKeySequence(Qt.Key_F10), UIAction(plugin.cmd_so)),
         DbgAction("SyncStepInto", QKeySequence(Qt.Key_F11), UIAction(plugin.cmd_si)),
-        DbgAction("SyncTranslate", QKeySequence(Qt.ALT + Qt.Key_F2), UIAction(plugin.cmd_translate)),
+        DbgAction(
+            "SyncTranslate",
+            QKeySequence(Qt.ALT | Qt.Key_F2),
+            UIAction(plugin.cmd_translate),
+        ),
         DbgAction("SyncBp", QKeySequence(Qt.Key_F2), UIAction(plugin.cmd_bp)),
-        DbgAction("SyncHwBp", QKeySequence(Qt.CTRL + Qt.Key_F2), UIAction(plugin.cmd_hwbp)),
-        DbgAction("SyncBpOneShot", QKeySequence(Qt.ALT + Qt.Key_F3), UIAction(plugin.cmd_bp1)),
-        DbgAction("SyncHwBpOneShot", QKeySequence(Qt.CTRL + Qt.Key_F3), UIAction(plugin.cmd_hwbp1))
-        )
+        DbgAction(
+            "SyncHwBp", QKeySequence(Qt.CTRL | Qt.Key_F2), UIAction(plugin.cmd_hwbp)
+        ),
+        DbgAction(
+            "SyncBpOneShot", QKeySequence(Qt.ALT | Qt.Key_F3), UIAction(plugin.cmd_bp1)
+        ),
+        DbgAction(
+            "SyncHwBpOneShot",
+            QKeySequence(Qt.CTRL | Qt.Key_F3),
+            UIAction(plugin.cmd_hwbp1),
+        ),
+    )
 
     for action in plugin_actions:
         UIAction.registerAction(action.name, action.key_seq)
         UIActionHandler.globalActions().bindAction(action.name, action.handler)
 
-    rs_log('commands added')
+    rs_log("commands added")
 
 
 retsync_plugin = SyncPlugin()
